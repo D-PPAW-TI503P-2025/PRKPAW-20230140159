3b517d987bfc0e5c891c15db2ab568e6bfb257b6
fikrimhammd <fikri.kdi60@gmail.com>
2025-11-13 15:46:30 +0700

    Memperbaiki 20230140159-server agar tidak menjadi submodule

---
+++ 20230140159-server/controllers/reportController.js
@@ -0,0 +1,27 @@
+const { Presensi } = require("../models");
+const { Op } = require("sequelize");
+
+
+exports.getDailyReport = async (req, res) => {
+  try {
+    const { nama } = req.query;
+    let options = { where: {} };
+
+    if (nama) {
+      options.where.nama = {
+        [Op.like]: `%${nama}%`,
+      };
+    }
+
+    const records = await Presensi.findAll(options);
+
+    res.json({
+      reportDate: new Date().toLocaleDateString(),
+      data: records,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Gagal mengambil laporan", error: error.message });
+  }
+};
